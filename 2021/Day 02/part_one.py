2fea3212edd76a91dcdb01614238d8aa8f58ce07
Pedro Boechat <pedroboechat@poli.ufrj.br>
2021-12-02 03:13:50 -0300

    feat: add 2021-02

---
+++ 2021/Day 02/part_one.py
@@ -0,0 +1,24 @@
+"""
+Advent of Code 2021
+Day 2 - Dive! (Part One)
+"""
+
+with open("./input.txt", encoding="utf-8") as file:
+    data = file.read().split("\n")
+
+horizontal = 0
+depth = 0
+
+for measurement in data:
+    command, amount = measurement.split(" ")
+    amount = int(amount)
+    if command == "forward":
+        horizontal += amount
+    elif command == "down":
+        depth += amount
+    else:
+        depth -= amount
+
+result = horizontal * depth
+
+print("Result =", result)
---
+++ 2021/Day 02/part_two.py
@@ -0,0 +1,26 @@
+"""
+Advent of Code 2021
+Day 2 - Dive! (Part Two)
+"""
+
+with open("./input.txt", encoding="utf-8") as file:
+    data = file.read().split("\n")
+
+aim = 0
+horizontal = 0
+depth = 0
+
+for measurement in data:
+    command, amount = measurement.split(" ")
+    amount = int(amount)
+    if command == "forward":
+        horizontal += amount
+        depth += aim * amount
+    elif command == "down":
+        aim += amount
+    else:
+        aim -= amount
+
+result = horizontal * depth
+
+print("Result =", result)
