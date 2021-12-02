7c936cf30985eca56859c29486d98fd8da6b91af
Pedro Boechat <pedroboechat@poli.ufrj.br>
2021-12-02 03:14:18 -0300

    feat: add 2015-01

---
+++ 2015/Day 01/part_one.py
@@ -0,0 +1,17 @@
+"""
+Advent of Code 2015
+Day 1 - Not Quite List (Part One)
+"""
+
+with open("./input.txt", encoding="utf-8") as file:
+    data = file.read()
+
+floor = 0
+
+for i in data:
+    if i == "(":
+        floor += 1
+    else:
+        floor -= 1
+
+print("Result =", floor)
---
+++ 2015/Day 01/part_two.py
@@ -0,0 +1,20 @@
+"""
+Advent of Code 2015
+Day 1 - Not Quite List (Part Two)
+"""
+
+with open("./input.txt", encoding="utf-8") as file:
+    data = file.read()
+
+floor = 0
+
+for index in range(len(data)):
+    if data[index] == "(":
+        floor += 1
+    else:
+        floor -= 1
+    if floor < 0:
+        result = index + 1
+        break
+
+print("Result =", result)
