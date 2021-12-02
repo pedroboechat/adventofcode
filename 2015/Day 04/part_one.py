75eb69cf03302c0c09a821b685abeb68ffde6c6f
Pedro Boechat <pedroboechat@poli.ufrj.br>
2021-12-02 11:01:09 -0300

    feat: add 2015-04

---
+++ 2015/Day 04/part_one.py
@@ -0,0 +1,24 @@
+"""
+Advent of Code 2015
+Day 4 - The Ideal Stocking Stuffer (Part One)
+"""
+
+import hashlib
+
+with open("./input.txt", encoding="utf-8") as file:
+    data = file.read()
+
+
+def format_hash(secret, number) -> str:
+    return f"{secret}{number}".encode()
+
+
+value = 0
+
+while True:
+    hashed = hashlib.md5(format_hash(data, value))
+    if hashed.hexdigest().startswith("00000"):
+        break
+    value += 1
+
+print("Result =", value)
