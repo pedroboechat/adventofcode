30710b03823b41d9991944986fe4f94741c319f7
Pedro Boechat <pedroboechat@poli.ufrj.br>
2021-12-03 00:15:39 -0300

    feat: add 2015-06

---
+++ 2015/Day 06/part_two.py
@@ -0,0 +1,32 @@
+"""
+Advent of Code 2015
+Day 6 - Probably a Fire Hazard (Part Two)
+"""
+
+import numpy as np
+
+with open("./input.txt", encoding="utf-8") as file:
+    data = [(i[:i.index("through")-1], i[i.index("through"):]) for i in file.read().splitlines()]
+
+grid = np.zeros((1000, 1000))
+
+for command in data:
+    v1 = [int(i) for i in command[0][command[0].rindex(" ")+1:].split(",")]
+    v2 = [int(i) for i in command[1][command[1].rindex(" ")+1:].split(",")]
+    if command[0].startswith("turn on"):
+        for x in range(v1[0], v2[0]+1):
+            for y in range(v1[1], v2[1]+1):
+                grid[x][y] += 1
+    elif command[0].startswith("turn off"):
+        for x in range(v1[0], v2[0]+1):
+            for y in range(v1[1], v2[1]+1):
+                if grid[x][y] > 0:
+                    grid[x][y] -= 1
+    else:
+        for x in range(v1[0], v2[0]+1):
+            for y in range(v1[1], v2[1]+1):
+                grid[x][y] += 2
+
+brightness = int(np.sum(grid))
+
+print("Result =", brightness)
